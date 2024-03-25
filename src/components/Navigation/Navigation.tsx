e45fd4f7d0e6cf9ab7d103199491ee6b296a70cb
hpnguyen27 <hpnguyen27@pm.me>
2024-03-24 22:34:06 -0400

    Added menu icon instead of entire text

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './NavigationBar.module.css';
-import logo from '../../assets/Untitled.png'
-import { Link } from 'react-router-dom'; // Import Link
-
+import logo from '../../assets/Untitled.png';
+import { Link } from 'react-router-dom';
 
 const NavigationBar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className={styles.navbar}>
-      <Link to="/" className={styles.logo}>  {/* Use Link here */}
-        <img src={logo} alt="Logo" className={styles.logoImage}/>
+      <Link to="/" className={styles.logo}>
+        <img src={logo} alt="Logo" className={styles.logoImage} />
       </Link>
-      <div className={styles.menu}>
+      <div className={`${styles.menu} ${isOpen ? styles.open : ''}`}>
         <a href="/about">About</a>
         <a href="/blog">Blog</a>
         <a href="/contact">Contact</a>
       </div>
+      <div className={`${styles.burger} ${isOpen ? styles.open : ''}`} onClick={toggleMenu}>
+      <div className={styles.line}></div>
+      <div className={styles.line}></div>
+      <div className={styles.line}></div>
+    </div>
     </div>
   );
 };
