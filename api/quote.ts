71b60bb108213a5232b8cb7c9103dfa6faaf423b
hpnguyen27 <hpnguyen27@pm.me>
2024-08-09 12:18:15 -0400

    remove env from git

--- api/quote.ts
+++ api/quote.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
-    console.log('API function called'); // Add this log
     const response = await axios.get(process.env.REACT_APP_QUOTE_API_URL ?? '', {
       headers: {
         'x-rapidapi-host': process.env.REACT_APP_RAPIDAPI_HOST,
@@ -11,10 +10,8 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       },
       params: { language_code: 'en' }
     });
-    console.log('API response:', response.data); // Add this log
     res.status(200).json(response.data);
   } catch (error) {
-    console.error('Error in API function:', error);
     res.status(500).json({ error: 'An unexpected error occurred in the API function' });
   }
 }
